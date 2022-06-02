1ad9c29d1b192df46b6472aaee8323c37067ab0a
zaa-inab <chakroun.zainab.ismo2020@gmail.com>
2022-06-02 08:58:47 +0100

    Login Form Validation Added

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,8 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder,Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { Admin } from 'src/app/model/admin';
+
 import { AdminService } from 'src/app/service/admin.service';
 
 @Component({
@@ -12,14 +12,14 @@ import { AdminService } from 'src/app/service/admin.service';
 })
 export class LoginComponent implements OnInit {
 
-  public loginForm!: FormGroup;
+   loginForm: FormGroup;
 
   constructor( private formBuilder: FormBuilder ,private adminService: AdminService, private router : Router) { }
 
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
-      username:[''],
-      password:['']
+      username:['' , Validators.required],
+      password:['', Validators.required]
     })
   }
 
@@ -42,4 +42,6 @@ export class LoginComponent implements OnInit {
 
      )
   }
+
+  
 }
